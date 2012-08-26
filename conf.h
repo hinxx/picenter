39b80a014bfda5d4733ba09602752c98da8b99fb
Stephan Schindel <sts@tp1.rub.de>
2012-08-26 12:32:37 +0200

    font fix for pi

--- conf.h
+++ conf.h
@@ -11,8 +11,7 @@
 #define ENTRY_Y		10
 
 /* Font preferences */
-//#define FONT	    "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
-#define FONT	    "/usr/local/lib/X11/fonts/dejavu/DejaVuSansMono.ttf"
+#define FONT	    "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
 #define FONT2	    "/usr/share/fonts/truetype/freefont/FreeMono-Bold.ttf"
 #define FONTSIZE    60
 
