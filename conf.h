281d3dd64e73de70cd76cbe89acde8c0c055c8b8
Stephan Schindel <sts@tp1.rub.de>
2012-08-27 23:21:20 +0200

    color change

--- conf.h
+++ conf.h
@@ -8,28 +8,28 @@
 /* GUI options */
 #define AMOUNT_ENTRIES 	15
 #define ENTRY_X		150
-#define ENTRY_Y		150
+#define ENTRY_Y		50
 
 /* Font preferences */
-#define FONT	    "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
+#define FONT	    "/usr/share/fonts/truetype/freefont/FreeMono-Bold.ttf"
 #define FONT2	    "/usr/share/fonts/truetype/freefont/FreeMono-Bold.ttf"
 #define FONTSIZE    60
 
 /* GUI Colors */
-#define COLOR_BACK_R	0
-#define COLOR_BACK_G	0
-#define COLOR_BACK_B	0
-#define COLOR_FRONT_R	255
-#define COLOR_FRONT_G	0
-#define COLOR_FRONT_B	0
+#define COLOR_BACK_R	255 
+#define COLOR_BACK_G	255
+#define COLOR_BACK_B	255
+#define COLOR_FRONT_R	25
+#define COLOR_FRONT_G	25
+#define COLOR_FRONT_B	25
 #define COLOR_DIR_R	0
 #define COLOR_DIR_G	0
 #define COLOR_DIR_B	255
 #define COLOR_MARKED_R	100
 #define COLOR_MARKED_G	100
 #define COLOR_MARKED_B	100
-#define COLOR_PLUGIN_R	0
-#define COLOR_PLUGIN_G	200
+#define COLOR_PLUGIN_R	200
+#define COLOR_PLUGIN_G	0
 #define COLOR_PLUGIN_B	0
 
 /* Video plugin options */
