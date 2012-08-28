e4f393f54cd67d0f5c9a7f18e6d97d6c76079eb2
Stephan Schindel <sts@tp1.rub.de>
2012-08-28 13:23:12 +0200

    add conf.h explanations

--- conf.h
+++ conf.h
@@ -2,17 +2,17 @@
 #define CONF_H
 
 /* OMXPlayer options */
-#define ADEV		"hdmi" // hdmi/local
-#define ADDCMD		" --refresh"
+#define ADEV		"hdmi"		// audio output: hdmi/local
+#define ADDCMD		" --refresh"	// additional commands, -r is a workaround for omxplayer issue #22
 
 /* GUI options */
-#define AMOUNT_ENTRIES 	15
-#define ENTRY_X		150
-#define ENTRY_Y		50
+#define AMOUNT_ENTRIES 	15	//  number of entries
+#define ENTRY_X		150	//  offset x for an entry 
+#define ENTRY_Y		50	//  offset y for an entry
 
 /* Font preferences */
-#define FONT	    "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
-#define FONT2	    "/usr/share/fonts/truetype/freefont/FreeMono-Bold.ttf"
+#define FONT	    "/usr/share/fonts/truetype/freefont/FreeMono.ttf"	    // default font
+#define FONT2	    "/usr/share/fonts/truetype/freefont/FreeMono-Bold.ttf"  // second font, currently unused
 #define FONTSIZE    60
 
 /* GUI Colors */
@@ -33,9 +33,9 @@
 #define COLOR_PLUGIN_B	0
 
 /* Video plugin options */
-#define PLUGIN_VIDEO_HOME   "/"
+#define PLUGIN_VIDEO_HOME   "/"	    // root for video files, default directory on plugin load
 
 /* Misc */
-#define WAIT_TIME 20
+#define WAIT_TIME 20	// waiting time between input events to lower CPU useage
 
 #endif
